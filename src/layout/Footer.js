7612e9b68d93ea71a6699efc0492f652f04d8b45
Charles Carrier <charles.e.carrier@gmail.com>
2019-03-26 13:31:34 +0800

    uploading basic footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -10,9 +10,25 @@ class Footer extends React.Component {
 
   render() {
     return (
-      <footer className="footer mt-auto py-3 fixed-bottom bg-dark">
-        <span className="text-muted">Hello from the footer</span>
-      </footer>
+      <div className="navbar pt-0 navbar-expand-md navbar-dark fixed-bottom bg-dark">
+        <ul className="navbar-nav mr-auto mb-0 ">
+          <li className="navbar-item">
+            <p className="nav-link">Security</p>
+          </li>
+          <li className="navbar-item">
+            <p className="nav-link">Privacy-Policy</p>
+          </li>
+          <li className="navbar-item">
+            <p className="nav-link">Contact Us</p>
+          </li>
+          <li className="navbar-item">
+            <p className="nav-link">Our Partners</p>
+          </li>
+          <li className="navbar-item">
+            <p className="nav-link">Cialfo vs BridgeU</p>
+          </li>
+        </ul>
+      </div>
     );
   }
 }
