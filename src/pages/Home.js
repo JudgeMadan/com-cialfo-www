7694e14db8f41f4e033057049c50118a7fa788eb
Charles Carrier <charles.e.carrier@gmail.com>
2019-03-27 13:28:50 +0800

    working on localization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,38 @@
 import React from "react";
-
+import * as contentful from "contentful";
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      testArticles: [],
+      locale: "zh-CN"
+    };
+  }
+
+  client = contentful.createClient({
+    space: "kn93hfefankj",
+    accessToken:
+      "bddb1871044902e088b9aec331fca83c23351f0f2c390633d7a8e1b428317981"
+  });
+
+  componentDidMount() {
+    this.fetchTestArticles().then(this.setTestArticles);
+  }
+
+  fetchTestArticles = () =>
+    this.client.getEntries({
+      content_type: "testArticleGlobal",
+      locale: this.state.locale
+    });
+
+  setTestArticles = response => {
+    this.setState({
+      testArticles: response.items
+    });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <div className="jumbotron">
         <div className="container">
