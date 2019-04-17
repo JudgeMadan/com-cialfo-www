d5ca291a18161fb2e2ffe926a58e727fd0a6cfb2
Stephan Fuchs <sfuchs@fastmail.com>
2019-04-17 14:41:53 +0800

    Updated deploy directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "ciaflo-www-marketing"
 set :repo_url, 'git@github.com:cialfo/cialfo-www-marketing'
 set :pty, true
 
-set :deploy_to, '~/apps/www-marketing'
+set :deploy_to, '~/apps/marketing_cialfo_co'
 
 set :keep_releases, 2
 
