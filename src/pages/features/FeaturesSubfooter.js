7519871a79e7ec174aab9a64b15d4e8b0e06a0d8
Charles Carrier <charles.e.carrier@gmail.com>
2019-04-21 14:31:20 +0100

    getting FeaturesSubfooter styled

--- src/pages/features/FeaturesSubfooter.js
+++ src/pages/features/FeaturesSubfooter.js
@@ -12,17 +12,21 @@ class FeaturesSubfooter extends React.Component {
   render() {
     console.log(this.props);
     return (
-      <Container className="featuresSendPage ">
-        <Row id="testContentRed">
-          <Col>
-            <img src={this.props.img} />
-          </Col>
-          <Col>
-            <h1 className="primary_font sub_footer_quote">
-              "{this.props.quote}"
-            </h1>
-          </Col>
-        </Row>
+      <Container className="featuresSendPage subFooterPage">
+        <Container className="test">
+          <Row>
+            <Col>
+              <img src="https://via.placeholder.com/360x500" />
+
+              {/* <img src={this.props.img} /> */}
+            </Col>
+            <Col className="test">
+              <h1 className="primary_font sub_footer_quote">
+                "{this.props.quote}"
+              </h1>
+            </Col>
+          </Row>
+        </Container>
       </Container>
     );
   }
