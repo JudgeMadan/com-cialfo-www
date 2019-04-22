ee62219f9788f654852138e6597100ae49f42448
Charles Carrier <charles.e.carrier@gmail.com>
2019-04-22 13:31:12 +0100

    starting to work on demo page

--- src/pages/GetADemo.js
+++ src/pages/GetADemo.js
@@ -1,59 +1,52 @@
 import React from "react";
 import * as contentful from "contentful";
 import Container from "react-bootstrap/Container";
+import "./about/About.css";
+import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import "./about/About.css";
+import Button from "react-bootstrap/Button";
 
 class GetADemo extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "",
+      surname: "",
+      email: "",
+      phoneNumber: "",
+      organization: "",
+      studentAmount: ""
+    };
   }
-
-  client = contentful.createClient({
-    space: "1acwuo4zy8aa",
-    accessToken:
-      "c6080034f52655b2fdb9267c7c555bff17c0134a4ae75b646bb112d992b485b2"
-  });
-
-  componentDidUpdate(prevProps) {
-    if (prevProps.locale !== this.props.locale) {
-      this.fetchAboutContent().then(this.setAboutContent);
-    }
-  }
-
-  componentDidMount() {
-    this.fetchAboutContent().then(this.setAboutContent);
-  }
-
-  fetchAboutContent = () =>
-    this.client.getEntries({
-      content_type: "about",
-      locale: this.props.locale
-    });
-
-  setAboutContent = response => {
-    const aboutContent = response.items[0].fields;
-    for (let key in aboutContent) {
-      if (typeof aboutContent[key] === "string") {
-        this.setState({
-          [key]: aboutContent[key]
-        });
-      } else if (Array.isArray(aboutContent[key])) {
-        this.setState({
-          [key]: aboutContent[key]
-        });
-      } else {
-        this.setState({
-          [key]: aboutContent[key].fields.file.url
-        });
-      }
-    }
+  formChange = e => {
+    const { id, value } = e.target;
+    console.log(value);
+    this.setState({ [id]: value });
+    e.preventDefault();
   };
 
+  submitForm = () => {
+    console.log(this.state);
+  };
+
+  componentDidMount() {
+    const script = document.createElement("script");
+
+    script.src = "https://cialfo51705.activehosted.com/f/embed.php?id=1";
+    script.async = true;
+    script.type = "text/javascript";
+    script.charset = "utf-8";
+
+    document.body.appendChild(script);
+  }
+
   render() {
-    return <Container className="aboutPage"> HEY</Container>;
+    return (
+      <Container>
+        <div class="_form_1" />
+      </Container>
+    );
   }
 }
 
