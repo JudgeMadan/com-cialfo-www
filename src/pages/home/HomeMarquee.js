334b24861f63641f5b4a42c6fd8ae9a5e3cfd2ea
Charles Carrier <charles.e.carrier@gmail.com>
2019-04-30 15:27:29 +0800

    adding line styling

--- src/pages/home/HomeMarquee.js
+++ src/pages/home/HomeMarquee.js
@@ -1,5 +1,8 @@
 import React from "react";
 import HomeMarqueeList from "././homeMarquee/HomeMarqueeList";
+import BlueOval from "./BlueOval.png";
+import Line from "./Line.png";
+import Stroke10 from "./Stroke10.png";
 
 class HomeMarquee extends React.Component {
   render() {
@@ -9,6 +12,9 @@ class HomeMarquee extends React.Component {
           <HomeMarqueeList />
           <HomeMarqueeList />
         </div>
+        <img className="marquee-oval" src={BlueOval} />
+        <img className="marquee-line" src={Line} />
+        <img className="marquee-stroke10" src={Stroke10} />
       </div>
     );
   }
