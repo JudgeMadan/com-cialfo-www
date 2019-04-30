7e880b9a10476068cb2dd64bddc769897b608b9c
Charles Carrier <charles.e.carrier@gmail.com>
2019-04-30 19:44:46 +0800

    trying out mobile

--- src/pages/home/HomeMarquee.js
+++ src/pages/home/HomeMarquee.js
@@ -3,19 +3,22 @@ import HomeMarqueeList from "././homeMarquee/HomeMarqueeList";
 import BlueOval from "./BlueOval.png";
 import Line from "./Line.png";
 import Stroke10 from "./Stroke10.png";
+import MediaQuery from "react-responsive";
 
 class HomeMarquee extends React.Component {
   render() {
     return (
-      <div className="marquee">
-        <div className="marquee--inner">
-          <HomeMarqueeList />
-          <HomeMarqueeList />
+      <MediaQuery query="(min-device-width: 1224px)">
+        <div className="marquee">
+          <div className="marquee--inner">
+            <HomeMarqueeList />
+            <HomeMarqueeList />
+          </div>
+          <img className="marquee-oval" src={BlueOval} />
+          <img className="marquee-line" src={Line} />
+          <img className="marquee-stroke10" src={Stroke10} />
         </div>
-        <img className="marquee-oval" src={BlueOval} />
-        <img className="marquee-line" src={Line} />
-        <img className="marquee-stroke10" src={Stroke10} />
-      </div>
+      </MediaQuery>
     );
   }
 }
