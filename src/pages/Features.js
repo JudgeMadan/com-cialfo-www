7deaa0e32ec427f2a5b771fce93653444f9a175c
Charles Carrier <charles.e.carrier@gmail.com>
2019-05-19 08:01:23 +0800

    styling mobile features page

--- src/pages/Features.js
+++ src/pages/Features.js
@@ -99,7 +99,7 @@ class Features extends React.Component {
           <Row className="titleContainer">
             <h1 className="primary_font">{this.state.featuresTitle}</h1>
           </Row>
-          <Row className="featuresVideoEmbed">
+          <Row className="mobile-featuresVideoEmbed">
             <div>
               <img className="oval" src={Oval} />
               <img className="line" src={Line} />
@@ -117,99 +117,198 @@ class Features extends React.Component {
             </h1>
           </Row>
         </MediaQuery>
-        <Row className="featureRows">
-          <Col className="homePageFeaturesImage homePageFeaturesImageBackground">
-            <img className="homePageImg" src={Documents} />
-            <img
-              className="features-left-align-light-blue-background"
-              src={LightBlueRectangle}
-            />
-          </Col>
-          <Col className="homePageFeaturesText">
-            <div className="homePageFeaturesRightSideTextObject">
-              <Row>
-                <h1 className="primary_font">
-                  {this.state.featurePageFeaturesSendDocumentTitle}
-                </h1>
-              </Row>
-              <Row>
-                <p className="secondary_font">
-                  {this.state.featurePageFeaturesSendDocumentBlurb}
-                </p>
-              </Row>
-              <Row>
-                <Link
-                  className="homeFeatureLink homePageFeaturesSendDocumentLinkText"
-                  to="/features/send"
-                >
-                  {this.state.featurePageFeaturesSendDocumentLinkText}
-                </Link>
-              </Row>
-            </div>
-          </Col>
-        </Row>
-        <Row className="featureRows">
-          <Col className="homePageFeaturesText">
-            <div className="homePageFeaturesLeftSideTextObject">
-              <Row>
-                <h1 className="primary_font">
-                  {this.state.featurePageFeaturesLeverageTitle}
-                </h1>
-              </Row>
-              <Row>
-                <p className="secondary_font">
-                  {this.state.featurePageFeaturesLeverageBlurb}
-                </p>
-              </Row>
-              <Row>
-                <Link
-                  className="homeFeatureLink homePageFeaturesLeverageLinkText"
-                  to="/features/research"
-                >
-                  {this.state.featurePageFeaturesLeverageLinkText}
-                </Link>
-              </Row>
-            </div>
-          </Col>
-          <Col className="homePageFeaturesImage homePageFeaturesImageBackgroundReverse">
-            <img className="homePageImg" src={ResearchImage} />
-            <img
-              className="features-right-align-light-blue-background"
-              src={LightBlueRectangle}
-            />
-          </Col>
-        </Row>
-        <Row className="featureRows">
-          <Col className="homePageFeaturesImage homePageFeaturesImageBackground">
-            <img className="homePageImg" src={Reports} />
-            <img
-              className="features-left-align-light-blue-background"
-              src={LightBlueRectangle}
-            />
-          </Col>
-          <Col className="homePageFeaturesText">
-            <div className="homePageFeaturesRightSideTextObject">
-              <Row>
-                <h1 className="primary_font">
-                  {this.state.featurePageFeaturesDiscoverTitle}
-                </h1>
-              </Row>
-              <Row>
-                <p className="secondary_font">
-                  {this.state.featurePageFeaturesDiscoverBlurb}
-                </p>
-              </Row>
-              <Row>
-                <Link
-                  className="homeFeatureLink homePageFeaturesDiscoverLinkText"
-                  to="/features/documents"
-                >
-                  {this.state.featurePageFeaturesDiscoverLinkText}
-                </Link>
-              </Row>
-            </div>
-          </Col>
-        </Row>
+        {/* FULL SCREEN SEND DOCUMENTS*/}
+        <MediaQuery query="(min-device-width: 1224px)">
+          <Row className="featureRows">
+            <Col className="homePageFeaturesImage homePageFeaturesImageBackground">
+              <img className="homePageImg" src={Documents} />
+              <img
+                className="features-left-align-light-blue-background"
+                src={LightBlueRectangle}
+              />
+            </Col>
+            <Col className="homePageFeaturesText">
+              <div className="homePageFeaturesRightSideTextObject">
+                <Row>
+                  <h1 className="primary_font">
+                    {this.state.featurePageFeaturesSendDocumentTitle}
+                  </h1>
+                </Row>
+                <Row>
+                  <p className="secondary_font">
+                    {this.state.featurePageFeaturesSendDocumentBlurb}
+                  </p>
+                </Row>
+                <Row>
+                  <Link
+                    className="homeFeatureLink homePageFeaturesSendDocumentLinkText"
+                    to="/features/send"
+                  >
+                    {this.state.featurePageFeaturesSendDocumentLinkText}
+                  </Link>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </MediaQuery>
+        {/* MOBILE SEND DOCUMENTS*/}
+        <MediaQuery query="(max-device-width: 1223px)">
+          <Container className="mobile-bottom-border">
+            <Row className="mobile-features-image mt-5">
+              <img className="mobile-feature-page-img" src={Documents} />
+            </Row>
+            <Row className="mobile-featuresFeaturesText mt-3">
+              <div className="mobile-features-page-content-text">
+                <Row className="center-in-row">
+                  <h1 className="primary_font center-in-row">
+                    {this.state.featurePageFeaturesSendDocumentTitle}
+                  </h1>
+                </Row>
+                <Row>
+                  <p className="secondary_font">
+                    {this.state.featurePageFeaturesSendDocumentBlurb}
+                  </p>
+                </Row>
+                <Row className="mb-5">
+                  <Link
+                    className="homeFeatureLink homePageFeaturesSendDocumentLinkText"
+                    to="/features/send"
+                  >
+                    {this.state.featurePageFeaturesSendDocumentLinkText}
+                  </Link>
+                </Row>
+              </div>
+            </Row>
+          </Container>
+        </MediaQuery>
+        {/* FULL SCREEN LEVERAGE*/}
+        <MediaQuery query="(min-device-width: 1224px)">
+          <Row className="featureRows">
+            <Col className="homePageFeaturesText">
+              <div className="homePageFeaturesLeftSideTextObject">
+                <Row>
+                  <h1 className="primary_font">
+                    {this.state.featurePageFeaturesLeverageTitle}
+                  </h1>
+                </Row>
+                <Row>
+                  <p className="secondary_font">
+                    {this.state.featurePageFeaturesLeverageBlurb}
+                  </p>
+                </Row>
+                <Row>
+                  <Link
+                    className="homeFeatureLink homePageFeaturesLeverageLinkText"
+                    to="/features/research"
+                  >
+                    {this.state.featurePageFeaturesLeverageLinkText}
+                  </Link>
+                </Row>
+              </div>
+            </Col>
+            <Col className="homePageFeaturesImage homePageFeaturesImageBackgroundReverse">
+              <img className="homePageImg" src={ResearchImage} />
+              <img
+                className="features-right-align-light-blue-background"
+                src={LightBlueRectangle}
+              />
+            </Col>
+          </Row>
+        </MediaQuery>
+        {/* MOBILE SCREEN LEVERAGE*/}
+        <MediaQuery query="(max-device-width: 1223px)">
+          <Container className="mobile-bottom-border">
+            <Row className="mobile-features-image mt-5">
+              <img className="mobile-feature-page-img" src={ResearchImage} />
+            </Row>
+            <Row className="mobile-featuresFeaturesText mt-3">
+              <div className="mobile-features-page-content-text">
+                <Row className="center-in-row">
+                  <h1 className="primary_font">
+                    {this.state.featurePageFeaturesLeverageTitle}
+                  </h1>
+                </Row>
+                <Row>
+                  <p className="secondary_font">
+                    {this.state.featurePageFeaturesLeverageBlurb}
+                  </p>
+                </Row>
+                <Row className="mb-5">
+                  <Link
+                    className="homeFeatureLink homePageFeaturesLeverageLinkText"
+                    to="/features/research"
+                  >
+                    {this.state.featurePageFeaturesLeverageLinkText}
+                  </Link>
+                </Row>
+              </div>
+            </Row>
+          </Container>
+        </MediaQuery>
+        {/* FULL SCREEN DISCOVER*/}
+        <MediaQuery query="(min-device-width: 1224px)">
+          <Row className="featureRows">
+            <Col className="homePageFeaturesImage homePageFeaturesImageBackground">
+              <img className="homePageImg" src={Reports} />
+              <img
+                className="features-left-align-light-blue-background"
+                src={LightBlueRectangle}
+              />
+            </Col>
+            <Col className="homePageFeaturesText">
+              <div className="homePageFeaturesRightSideTextObject">
+                <Row>
+                  <h1 className="primary_font">
+                    {this.state.featurePageFeaturesDiscoverTitle}
+                  </h1>
+                </Row>
+                <Row>
+                  <p className="secondary_font">
+                    {this.state.featurePageFeaturesDiscoverBlurb}
+                  </p>
+                </Row>
+                <Row>
+                  <Link
+                    className="homeFeatureLink homePageFeaturesDiscoverLinkText"
+                    to="/features/documents"
+                  >
+                    {this.state.featurePageFeaturesDiscoverLinkText}
+                  </Link>
+                </Row>
+              </div>
+            </Col>
+          </Row>
+        </MediaQuery>
+        {/* MOBILE  DISCOVER*/}
+        <MediaQuery query="(max-device-width: 1223px)">
+          <Container className="mobile-bottom-border">
+            <Row className="mobile-features-image mt-5">
+              <img className="mobile-feature-page-img" src={Reports} />
+            </Row>
+            <Row className="homePageFeaturesText mt-3">
+              <div className="mobile-features-page-content-text">
+                <Row>
+                  <h1 className="primary_font">
+                    {this.state.featurePageFeaturesDiscoverTitle}
+                  </h1>
+                </Row>
+                <Row>
+                  <p className="secondary_font">
+                    {this.state.featurePageFeaturesDiscoverBlurb}
+                  </p>
+                </Row>
+                <Row className="mb-5">
+                  <Link
+                    className="homeFeatureLink homePageFeaturesDiscoverLinkText"
+                    to="/features/documents"
+                  >
+                    {this.state.featurePageFeaturesDiscoverLinkText}
+                  </Link>
+                </Row>
+              </div>
+            </Row>
+          </Container>
+        </MediaQuery>
       </Container>
     );
   }
