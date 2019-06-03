28a50db5614a61aea3db3c94eb2356a441e2d7e9
Charles Carrier <charles.e.carrier@gmail.com>
2019-06-03 17:02:23 +0800

    setting localization

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
         "country_code=US"
       ];
 
-      const country_codeArray = cookieArrayChina.filter(
+      const country_codeArray = cookieArray.filter(
         cookie => cookie.substring(0, 12) === "country_code"
       );
 
