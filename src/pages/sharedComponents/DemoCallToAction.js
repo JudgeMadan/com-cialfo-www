5bc1f6e6658b8a2eedf2d69d09cac2bc7a72e2e9
Thantham (Judge) Madan <judge.madan@gmail.com>
2019-08-06 17:19:48 +0800

    Demo Call To Action

---
+++ src/pages/sharedComponents/DemoCallToAction.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "../home/Home.css";
+import { Link } from "react-router-dom";
+
+class DemoCallToAction extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="demo-cta-container">
+        <Row className="demo-cta-content center-in-row">
+          <h1 className="primary_font demo-cta-title-text">
+              Ready to get started? Our team is here to help.
+          </h1>
+        </Row>
+        <Row className="demo-cta-content button">
+          <Link to="demo">
+            <button className="nav-link-button sharp-corners-button btn btn-primary submit_button">
+              Get started
+            </button>
+          </Link>
+        </Row>
+      </div>
+    );
+  }
+}
+export default DemoCallToAction;
