b08eef7a5811f756deee6f141d8cac638d694df1
Charles E Carrier <charles.e.carrier@gmail.com>
2019-08-06 23:00:06 -0400

    basic carousel working

--- src/pages/home/HomeCarousel.js
+++ src/pages/home/HomeCarousel.js
@@ -5,9 +5,26 @@ import ReactPlayer from "react-player";
 class HomeCarousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      playing_1: false,
+      muted_1: false,
+      playing_2: false,
+      playing_3: false,
+      playing_4: false,
+      playing_5: false,
+    };
   }
 
+  afterChangeHandler = (video) => {
+    this.setState({
+      playing_1: false,
+      muted_1: true,
+      playing_2: false,
+      playing_3: false,
+      playing_4: false,
+      playing_5: false,
+    })
+  }
 
   render() {
     let settings = {
@@ -15,36 +32,60 @@ class HomeCarousel extends React.Component {
       infinite: true,
       speed: 500,
       slidesToShow: 1,
-      centerPadding: "60px",
+
+      // BASIC NOT FULL SCREEN CAROUSEL
+      // centerPadding: "200px",
+      centerPadding: "250px",
       slidesToScroll: 1,
       centerMode: true,
-      focusOnSelect: true
+      focusOnSelect: true,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            centerPadding: "200px",
+          }
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            centerPadding: "150px",
+          }
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            centerPadding: "100px",
+          }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            centerPadding: "50px",
+          }
+        }
+      ]
     };
     return (
-      <div>
-        <Slider {...settings} className="slider-container mx-auto">
+      <div className="slider-outer-container">
+        <Slider {...settings} beforeChange={this.afterChangeHandler} className="slider-container mx-auto">
           <div className="slider-element">
             <ReactPlayer
+              onClick={this.afterChangeHandler}
               className="video mx-auto"
               width="600px"
               height="366px"
               url={this.props.homePageVideoCaseStudyVideoEmbed}
+              playing={this.state.playing_1}
             />
           </div>
-          <div className="slider-element">
+          <div className="slider-element hello">
             <ReactPlayer
               className="video mx-auto"
               width="600px"
               height="366px"
               url={this.props.homePageVideoCaseStudyVideoEmbed}
-            />
-          </div>
-          <div className="slider-element mx-auto">
-            <ReactPlayer
-              className="video mx-auto"
-              width="600px"
-              height="366px"
-              url={this.props.homePageVideoCaseStudyVideoEmbed}
+              playing={this.state.playing_2}
             />
           </div>
           <div className="slider-element mx-auto">
@@ -53,6 +94,7 @@ class HomeCarousel extends React.Component {
               width="600px"
               height="366px"
               url={this.props.homePageVideoCaseStudyVideoEmbed}
+              playing={this.state.playing_3}
             />
           </div>
           <div className="slider-element mx-auto">
@@ -61,6 +103,16 @@ class HomeCarousel extends React.Component {
               width="600px"
               height="366px"
               url={this.props.homePageVideoCaseStudyVideoEmbed}
+              playing={this.state.playing_4}
+            />
+          </div>
+          <div className="slider-element mx-auto">
+            <ReactPlayer
+              className="video mx-auto"
+              width="600px"
+              height="366px"
+              url={this.props.homePageVideoCaseStudyVideoEmbed}
+              playing={this.state.playing_5}
             />
           </div>
         </Slider>
