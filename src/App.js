a6c8045dce20b0ff46ace2f328eea0b4c6bcb68a
Charles E Carrier <charles.e.carrier@gmail.com>
2019-08-14 08:15:16 -0400

    refactoring app.js

--- src/App.js
+++ src/App.js
@@ -2,182 +2,16 @@ import React, { Component } from "react";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import Router from "./layout/Router";
+import { DataContext } from "./contexts/DataContext"
 
 class App extends Component {
+  static contextType = DataContext;
+
   constructor(props) {
     super(props);
     this.state = {
-      space: "2w8l1bcem16l",
-      accessToken: "bO1jaDXJM1S5kWXDVJoZ6buysg9bGkhohqYyJr-NxIw",
-      environment: "master",
-      locale: "en-US",
-      getADemoEmail: "",
-      chinaSpace: {
-        space: "1acwuo4zy8aa",
-        accessToken:
-          "c6080034f52655b2fdb9267c7c555bff17c0134a4ae75b646bb112d992b485b2",
-        spaceName: "china"
-      },
-      indiaSpace: {
-        space: "6s1t375h60iy",
-        accessToken: "vZ4pPEGukFHPrZCLU0ql6SlH5hvabD-aAV2wr65Pjwo",
-        spaceName: "india"
-      },
-      internationalSpace: {
-        space: "kq0n6h3xq8i9",
-        accessToken: "9tSaFiRLObn_CKT5hpYU-iNrTN47rUquWSmSfV3KNLY",
-        spaceName: "intl"
-      },
-      usaSpace: {
-        space: "2w8l1bcem16l",
-        accessToken: "bO1jaDXJM1S5kWXDVJoZ6buysg9bGkhohqYyJr-NxIw",
-        spaceName: "us"
-      },
-      spaces: {
-        cn: {
-          space: "1acwuo4zy8aa",
-          accessToken:
-            "c6080034f52655b2fdb9267c7c555bff17c0134a4ae75b646bb112d992b485b2"
-        },
-        intl: {
-          space: "kq0n6h3xq8i9",
-          accessToken: "9tSaFiRLObn_CKT5hpYU-iNrTN47rUquWSmSfV3KNLY"
-        },
-        india: {
-          space: "6s1t375h60iy",
-          accessToken: "vZ4pPEGukFHPrZCLU0ql6SlH5hvabD-aAV2wr65Pjwo"
-        },
-        us: {
-          space: "2w8l1bcem16l",
-          accessToken: "bO1jaDXJM1S5kWXDVJoZ6buysg9bGkhohqYyJr-NxIw"
-        }
-      }
-    };
-  }
-
-  data = {
-    environment: "staging",
-    chinaSpace: {
-      space: "1acwuo4zy8aa",
-      accessToken:
-        "c6080034f52655b2fdb9267c7c555bff17c0134a4ae75b646bb112d992b485b2",
-      spaceName: "china"
-    },
-    indiaSpace: {
-      space: "6s1t375h60iy",
-      accessToken: "vZ4pPEGukFHPrZCLU0ql6SlH5hvabD-aAV2wr65Pjwo",
-      spaceName: "india"
-    },
-    internationalSpace: {
-      space: "kq0n6h3xq8i9",
-      accessToken: "9tSaFiRLObn_CKT5hpYU-iNrTN47rUquWSmSfV3KNLY",
-      spaceName: "intl"
-    },
-    usaSpace: {
-      space: "2w8l1bcem16l",
-      accessToken: "bO1jaDXJM1S5kWXDVJoZ6buysg9bGkhohqYyJr-NxIw",
-      spaceName: "us"
-    },
-  }
-
-  setFeatures = response => {
-    this.setState({
-      content: response
-    })
-  }
-
-
-
-  componentDidMount() {
-    let space = '';
-    let accessToken = '';
-    // getAllCopy(this.space, "master", "bO1jaDXJM1S5kWXDVJoZ6buysg9bGkhohqYyJr-NxIw").then(this.setFeatures)
-    if (document.cookie) {
-      const cookieArray = document.cookie.split(" ");
-      // KEEP cookieArrays for now for testing purposes
-      const cookieArrayChina = [
-        "ajs_user_id=null;",
-        "ajs_group_id=null;",
-        "ajs_anonymous_id=%22da02155a-24b5-4a2f-975b-57a2d9b11ba7%22;",
-        "__distillery=37c1813_175da9d6-de81-42cc-a6bd-df41c410e0ac-7e613e6a1-52e74d9ca234-69d2;",
-        "intercom-id-giyujuw5=0732defb-3725-488f-809e-2b74254a709a;",
-        "country_code=CN"
-      ];
-      const cookieArrayIndia = [
-        "ajs_user_id=null;",
-        "ajs_group_id=null;",
-        "ajs_anonymous_id=%22da02155a-24b5-4a2f-975b-57a2d9b11ba7%22;",
-        "__distillery=37c1813_175da9d6-de81-42cc-a6bd-df41c410e0ac-7e613e6a1-52e74d9ca234-69d2;",
-        "intercom-id-giyujuw5=0732defb-3725-488f-809e-2b74254a709a;",
-        "country_code=IN"
-      ];
-      const cookieArrayUSA = [
-        "ajs_user_id=null;",
-        "ajs_group_id=null;",
-        "ajs_anonymous_id=%22da02155a-24b5-4a2f-975b-57a2d9b11ba7%22;",
-        "__distillery=37c1813_175da9d6-de81-42cc-a6bd-df41c410e0ac-7e613e6a1-52e74d9ca234-69d2;",
-        "intercom-id-giyujuw5=0732defb-3725-488f-809e-2b74254a709a;",
-        "country_code=US"
-      ];
-      const cookieArrayInternational = [
-        "ajs_user_id=null;",
-        "ajs_group_id=null;",
-        "ajs_anonymous_id=%22da02155a-24b5-4a2f-975b-57a2d9b11ba7%22;",
-        "__distillery=37c1813_175da9d6-de81-42cc-a6bd-df41c410e0ac-7e613e6a1-52e74d9ca234-69d2;",
-        "intercom-id-giyujuw5=0732defb-3725-488f-809e-2b74254a709a;",
-        "country_code=JP"
-      ];
-
-      const country_codeArray = cookieArray.filter(
-        cookie => cookie.substring(0, 12) === "country_code"
-      );
-
-      const country_code = country_codeArray[0];
-      if (country_code === "country_code=US") {
-        space = this.data.usaSpace.space
-        accessToken = this.data.usaSpace.accessToken
-      }
-
-      if (country_code === "country_code=CN") {
-        window.location.href = "https://cialfo.cn"
-      } else if (country_code === "country_code=IN") {
-        space = this.data.indiaSpace.space
-        accessToken = this.data.indiaSpace.accessToken
-        this.setState({
-          space: this.state.indiaSpace.space,
-          accessToken: this.state.indiaSpace.accessToken,
-          country_code: country_code,
-          spaceName: this.state.indiaSpace.spaceName
-        });
-      } else if (country_code === "country_code=US") {
-        space = this.data.usaSpace.space
-        accessToken = this.data.usaSpace.accessToken
-        this.setState({
-          space: this.state.usaSpace.space,
-          accessToken: this.state.usaSpace.accessToken,
-          country_code: country_code,
-          spaceName: this.state.usaSpace.spaceName
-        });
-      } else {
-        space = this.data.internationalSpace.space
-        accessToken = this.data.internationalSpace.accessToken
-        this.setState({
-          space: this.state.internationalSpace.space,
-          accessToken: this.state.internationalSpace.accessToken,
-          country_code: country_code,
-          spaceName: this.state.internationalSpace.spaceName
-        });
-      }
-    } else {
-      space = this.data.internationalSpace.space
-      accessToken = this.data.internationalSpace.accessToken
-      this.setState({
-        space: this.state.internationalSpace.space,
-        accessToken: this.state.internationalSpace.accessToken,
-        spaceName: this.state.internationalSpace.spaceName
-      });
     }
-  }
+  };
 
   sendEmailAddressToGetADemo = email => {
     this.setState({
@@ -191,38 +25,15 @@ class App extends Component {
     });
   };
 
-  setSpace = url => {
-    if (url === "cn") {
-      window.location.href = "https://cialfo.cn"
-    } else if (url === "intl") {
-      return this.state.spaces.intl.space;
-    } else if (url === "in") {
-      return this.state.spaces.india.space;
-    } else if (url === "us") {
-      return this.state.spaces.us.space;
-    }
-  };
-
-  setAccessToken = url => {
-    if (url === "cn") {
-      return this.state.spaces.cn.accessToken;
-    } else if (url === "intl") {
-      return this.state.spaces.intl.accessToken;
-    } else if (url === "in") {
-      return this.state.spaces.india.accessToken;
-    } else if (url === "us") {
-      return this.state.spaces.us.accessToken;
-    }
-  };
 
   render() {
-    const spaceName = this.state.spaceName
+    const spaceName = this.context.generateSpaceAndAccess().spaceName
     if (spaceName) {
       return (
         <div>
           <Header />
           <Router
-            spaceName={this.state.spaceName}
+            spaceName={spaceName}
             getADemoEmail={this.state.getADemoEmail}
             sendEmailAddressToGetADemo={this.sendEmailAddressToGetADemo}
           />
