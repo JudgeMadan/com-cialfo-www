1b7c9e27e2d9d047fae5e29087cb79b32ca7bf50
Mariano Salvador <skrullruby.dev@yahoo.com>
2019-08-19 16:13:51 -0300

    update style of demo call to action

--- src/pages/sharedComponents/DemoCallToAction.js
+++ src/pages/sharedComponents/DemoCallToAction.js
@@ -13,11 +13,11 @@ class DemoCallToAction extends React.Component {
     return (
       <div className="demo-cta-container">
         <Row className="demo-cta-content center-in-row">
-          <h1 className="primary_font demo-cta-title-text">
+          <h1 className="primary_font demo-cta-title-text text-white">
               Ready to get started? Our team is here to help.
           </h1>
         </Row>
-        <Row className="demo-cta-content button">
+        <Row className="demo-cta-content">
           <Link to="demo">
             <button className="nav-link-button sharp-corners-button btn btn-primary submit_button">
               Get started
