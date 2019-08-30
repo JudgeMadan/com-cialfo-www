71de51f2b702f90a1a66c8fb00666e258de80878
Charles E Carrier <charles.e.carrier@gmail.com>
2019-08-30 18:01:59 +0800

    turning on cookie

--- src/contexts/DataContext.js
+++ src/contexts/DataContext.js
@@ -45,7 +45,7 @@ class DataContextProvider extends Component {
         "country_code=JP"
       ];
 
-      const country_codeArray = cookieArrayUSA.filter(
+      const country_codeArray = cookieArray.filter(
         cookie => cookie.substring(0, 12) === "country_code"
       );
 
